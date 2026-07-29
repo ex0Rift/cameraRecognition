6aef71a6138075601d1dd9a4fcffcedab3e57007
Nate <daviesnathanael45@gmail.com>
2026-07-29 19:01:28 +0100

    updated how objects that need x and y work for my convenience by having a Vector class containing x and y. added a ball that moves like a pong pong ball. so far collides with the sides

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import cv2 , math
 import mediapipe as mp
 
-#
+# create vector class for objects using x and y
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 # initialise mediapipe
 #
 # face detection
@@ -19,9 +24,11 @@ mode = 1
 
 frameFail = False
 camera = -1
+ball = Vector(0,0)
+force = Vector(20,20)
 
 def main():
-    global camera , frameFail
+    global camera , frameFail , ball , force
     #loops a total of 4 times to try 4 different camera options and tries to open them
     for i in range (0,4):
         # make the video capture device
@@ -76,11 +83,27 @@ def main():
                         one_index = result.multi_hand_landmarks[0].landmark[8]
                         two_index = result.multi_hand_landmarks[1].landmark[8]
                         # normalise the coordinates to the screen
-                        one_index_n = {"x":int(one_index.x * w), "y":int(one_index.y * h)}
-                        two_index_n = {"x":int(two_index.x * w), "y":int(two_index.y * h)}
+                        one_index_n = Vector(int(one_index.x * w), int(one_index.y * h))
+                        two_index_n = Vector(int(two_index.x * w), int(two_index.y * h))
 
                         #find the distance and normalise it to a useable number
                         distance = int(math.sqrt((two_index.x - one_index.x)**2 + (two_index.y - one_index.y)**2)*1000)
+
+            # logic for the ball
+            #
+
+            #move the ball by force
+            ball.x += force.x
+            ball.y += force.y
+
+            #collisions with screen sides
+            if ball.y >= h or ball.y <= 0:
+                force.y = -force.y
+            if ball.x >= w or ball.x <= 0:
+                force.x = -force.x
+
+            #collision with the line between index fingers
+            
             #DRAWING
             #
             # draw according to which detection mode needing specific drawing requirments
@@ -96,7 +119,7 @@ def main():
                         # only does these drawings if both hands are detected
                         if len(result.multi_hand_landmarks) >= 2:
                             # draws the line between index fingers
-                            cv2.line(frame, (one_index_n["x"],one_index_n["y"]),(two_index_n["x"],two_index_n["y"]),(255,0,0),2)
+                            cv2.line(frame, (one_index_n.x,one_index_n.y),(two_index_n.x,two_index_n.y),(255,0,0),2)
                         else:
                             # tell user both hands need to be on the screen
                             cv2.putText(frame,"Show BOTH Hands!",(10,50),cv2.FONT_HERSHEY_DUPLEX,1,(0,0,255),3)
@@ -105,6 +128,8 @@ def main():
                         cv2.putText(frame,"Show Hands!",(10,50),cv2.FONT_HERSHEY_DUPLEX,1,(0,0,255),3)
             # always tell the user that q can be pressed to quit the program
             cv2.putText(frame,"Press Q to quit",(w-260,h-25),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255),2)
+            # draw the ball to the screen
+            cv2.circle(frame,(ball.x,ball.y),40,(255,255,0),-1)
 
             # show the webcam
             cv2.imshow('webcam test',frame)
